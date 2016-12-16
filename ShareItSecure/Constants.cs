c607547b6e66de944f10d48aec164dbd4d5a10a5
Tigran Karapetyan <tkarapetyan95@outlook.com>
2016-12-16 16:42:42 +0400

    Add project files.

---
+++ ShareItSecure/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ShareItSecure
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://shareitsecure.azurewebsites.net";
+	}
+}
+
